e1c268b35df6758f66b1e9734294dd9b20892a0f
Ali <ali.kasymkhan_w@mail.ru>
2024-01-18 16:12:29 +0600

    made by helpers (для обработки ошибки )

--- pkg/helpers/logging_helpers.go
+++ pkg/helpers/logging_helpers.go
@@ -1,1 +1,8 @@
 package helpers
+
+import "testProject/pkg/logging"
+
+func LogAndReturnError(logger *logging.Logger, message string, err error) error {
+	logger.Errorf("%s: %v", message, err)
+	return err
+}
