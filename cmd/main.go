4d356a52cd9a745956539978141f7509cb077385
Ali <ali.kasymkhan_w@mail.ru>
2024-01-18 15:50:06 +0600

    Made basic structure

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	// TODO: config - cleanenv
+	// TODO: logger logrus
+	// TODO: db sqlx || pq
+	// TODO: repo
+	// TODO: service
+	// TODO: router gin
+	// listen
+
+	// + migrate, docker, makefile,..
+}
