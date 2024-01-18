c99da9f3483864468f1d6f6940dbb6e62f370e0f
Ali <ali.kasymkhan_w@mail.ru>
2024-01-18 16:38:27 +0600

    ran the code through the tool (go vet), assembled the service in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testProject/internal/config"
+	"testProject/internal/handlers"
+	"testProject/pkg/logging"
+	"testProject/repository"
+	"testProject/service"
+
+	_ "github.com/jmoiron/sqlx"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 
-	// TODO: config - cleanenv
-	// TODO: logger logrus
-	// TODO: db sqlx || pq
-	// TODO: repo
-	// TODO: service
-	// TODO: router gin
-	// listen
+	cfg := config.GetConfig()
 
-	// + migrate, docker, makefile,..
+	logger := logging.GetLogger()
+
+	dbURL := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+		cfg.DB.Host, cfg.DB.User, cfg.DB.Password, cfg.DB.Name, cfg.DB.Port)
+
+	repo, err := repository.NewRepository(dbURL, logger)
+	if err != nil {
+		logger.Fatalf("Failed to create repository: %v", err)
+		return
+	}
+	service := service.NewService(repo, logger)
+
+	router := gin.Default()
+
+	handlers.RegisterRoutes(router, service)
+
+	port := cfg.App.Port
+	addr := fmt.Sprintf(":%d", port)
+
+	logger.Info("Starting server at", addr)
+	if err := http.ListenAndServe(addr, router); err != nil {
+		logger.Fatal("Failed to start server:", err)
+		os.Exit(1)
+	}
+
 }
